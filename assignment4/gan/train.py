7e81c2dc68dc44335ca5bd94300dfa36da3fd4bf
HariniAru <35876094+HariniAru@users.noreply.github.com>
2024-04-11 23:58:59 -0500

    Update train.py

--- assignment4/gan/train.py
+++ assignment4/gan/train.py
@@ -1,8 +1,8 @@
+import torch
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 from gan.utils import sample_noise, show_images, deprocess_img, preprocess_img
-
 
 def train(
     D,
@@ -20,70 +20,59 @@ def train(
 ):
     """
     Train loop for GAN.
-
-    The loop will consist of two steps: a discriminator step and a generator step.
-
-    (1) In the discriminator step, you should zero gradients in the discriminator
-    and sample noise to generate a fake data batch using the generator. Calculate
-    the discriminator output for real and fake data, and use the output to compute
-    discriminator loss. Call backward() on the loss output and take an optimizer
-    step for the discriminator.
-
-    (2) For the generator step, you should once again zero gradients in the generator
-    and sample noise to generate a fake data batch. Get the discriminator output
-    for the fake data batch and use this to compute the generator loss. Once again
-    call backward() on the loss and take an optimizer step.
-
-    You will need to reshape the fake image tensor outputted by the generator to
-    be dimensions (batch_size x input_channels x img_size x img_size).
-
-    Use the sample_noise function to sample random noise, and the discriminator_loss
-    and generator_loss functions for their respective loss computations.
-
-
-    Inputs:
-    - D, G: PyTorch models for the discriminator and generator
-    - D_solver, G_solver: torch.optim Optimizers to use for training the
-      discriminator and generator.
-    - discriminator_loss, generator_loss: Functions to use for computing the generator and
-      discriminator loss, respectively.
-    - show_every: Show samples after every show_every iterations.
-    - batch_size: Batch size to use for training.
-    - noise_size: Dimension of the noise to use as input to the generator.
-    - num_epochs: Number of epochs over the training dataset to use for training.
-    - train_loader: image dataloader
-    - device: PyTorch device
+    - D, G: Discriminator and generator PyTorch models
+    - D_solver, G_solver: Optimizers for D and G
+    - discriminator_loss, generator_loss: Loss functions for D and G
+    - show_every: How often to show generated images
+    - batch_size: Batch size
+    - noise_size: Dimension of the input noise vector
+    - num_epochs: Number of epochs to train for
+    - train_loader: DataLoader for the dataset
+    - device: Device on which to train
     """
+
     iter_count = 0
     for epoch in range(num_epochs):
         print("EPOCH: ", (epoch + 1))
         for x, _ in train_loader:
-            _, input_channels, img_size, _ = x.shape
-
-            real_images = preprocess_img(x).to(device)  # normalize
-
-            # Store discriminator loss output, generator loss output, and fake image output
-            # in these variables for logging and visualization below
-            d_error = None
-            g_error = None
-            fake_images = None
-
-            ####################################
-            #          YOUR CODE HERE          #
-            ####################################
-
-            ##########       END      ##########
-
-            # Logging and output visualization
+            D.train()
+            G.train()
+            
+            # Reset gradients
+            D_solver.zero_grad()
+            
+            real_images = preprocess_img(x).to(device)
+            logits_real = D(real_images)
+            
+            g_fake_seed = sample_noise(batch_size, noise_size, device=device)
+            fake_images = G(g_fake_seed).detach()
+            logits_fake = D(fake_images)
+            
+            # Calculate discriminator loss and take optimizer step
+            d_total_error = discriminator_loss(logits_real, logits_fake)
+            d_total_error.backward()
+            D_solver.step()
+            
+            # Generator step
+            G_solver.zero_grad()
+            g_fake_seed = sample_noise(batch_size, noise_size, device=device)
+            fake_images = G(g_fake_seed)
+            
+            # Recalculate discriminator logits on fake images
+            gen_logits_fake = D(fake_images)
+            g_error = generator_loss(gen_logits_fake)  # calculate generator loss
+            g_error.backward()
+            G_solver.step()
+            
+            d_error = d_total_error.item()
+            g_error = g_error.item()
+            
             if iter_count % show_every == 0:
-                print(
-                    "Iter: {}, D: {:.4}, G:{:.4}".format(
-                        iter_count, d_error.item(), g_error.item()
-                    )
-                )
-                disp_fake_images = deprocess_img(fake_images.data)  # denormalize
-                imgs_numpy = (disp_fake_images).cpu().numpy()
-                show_images(imgs_numpy[0:16], color=input_channels != 1)
+                print(f"Iter: {iter_count}, D: {d_error:.4f}, G:{g_error:.4f}")
+                fake_images = fake_images.view(batch_size, input_channels, img_size, img_size)
+                fake_images = deprocess_img(fake_images.data.cpu())
+                show_images(fake_images[0:16], color=input_channels != 1)
                 plt.show()
                 print()
+            
             iter_count += 1
