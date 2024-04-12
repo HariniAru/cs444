2487b5385ba0a4b771efbfb52542e63e6f5c5a79
HariniAru <35876094+HariniAru@users.noreply.github.com>
2024-04-11 23:56:09 -0500

    Update models.py

--- assignment4/gan/models.py
+++ assignment4/gan/models.py
@@ -1,44 +1,62 @@
 import torch
+import torch.nn as nn
 
-
-class Discriminator(torch.nn.Module):
+class Discriminator(nn.Module):
     def __init__(self, input_channels=3):
         super(Discriminator, self).__init__()
-
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-
-        ##########       END      ##########
+        # Assuming the input images are 64x64
+        self.main = nn.Sequential(
+            # input is (input_channels) x 64 x 64
+            nn.Conv2d(input_channels, 64, 4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 64 x 32 x 32
+            nn.Conv2d(64, 128, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 128 x 16 x 16
+            nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 256 x 8 x 8
+            nn.Conv2d(256, 512, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 512 x 4 x 4
+            nn.Conv2d(512, 1, 4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-
-        ##########       END      ##########
-
-        return x
+        output = self.main(x)
+        return output.view(-1, 1).squeeze(1)
 
 
-class Generator(torch.nn.Module):
+class Generator(nn.Module):
     def __init__(self, noise_dim, output_channels=3):
         super(Generator, self).__init__()
-        self.noise_dim = noise_dim
-
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-
-        ##########       END      ##########
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(noise_dim, 512, 4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 4 x 4
+            nn.ConvTranspose2d(512, 256, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # state size. 256 x 8 x 8
+            nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # state size. 128 x 16 x 16
+            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # state size. 64 x 32 x 32
+            nn.ConvTranspose2d(64, output_channels, 4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+            # state size. (output_channels) x 64 x 64
+        )
 
     def forward(self, x):
-
-        ####################################
-        #          YOUR CODE HERE          #
-        ####################################
-
-        ##########       END      ##########
-
-        return x
+        output = self.main(x)
+        return output
