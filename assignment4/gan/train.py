f06f6d4aa810e61b703b1f9ed6a4a37378153744
HariniAru <35876094+HariniAru@users.noreply.github.com>
2024-04-16 15:12:51 -0500

    Update train.py

--- assignment4/gan/train.py
+++ assignment4/gan/train.py
@@ -1,4 +1,3 @@
-import torch
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
@@ -20,61 +19,56 @@ def train(
 ):
     """
     Train loop for GAN.
-    - D, G: Discriminator and generator PyTorch models
-    - D_solver, G_solver: Optimizers for D and G
-    - discriminator_loss, generator_loss: Loss functions for D and G
-    - show_every: How often to show generated images
-    - batch_size: Batch size
-    - noise_size: Dimension of the input noise vector
-    - num_epochs: Number of epochs to train for
-    - train_loader: DataLoader for the dataset
-    - device: Device on which to train
-    """
 
+    The loop will consist of two steps: a discriminator step and a generator step.
+
+    Inputs:
+    - D, G: PyTorch models for the discriminator and generator
+    - D_solver, G_solver: torch.optim Optimizers to use for training the
+      discriminator and generator.
+    - discriminator_loss, generator_loss: Functions to use for computing the generator and
+      discriminator loss, respectively.
+    - show_every: Show samples after every show_every iterations.
+    - batch_size: Batch size to use for training.
+    - noise_size: Dimension of the noise to use as input to the generator.
+    - num_epochs: Number of epochs over the training dataset to use for training.
+    - train_loader: image dataloader
+    - device: PyTorch device
+    """
     iter_count = 0
     for epoch in range(num_epochs):
         print("EPOCH: ", (epoch + 1))
         for x, _ in train_loader:
-            D.train()
-            G.train()
-            
-            # Reset gradients
-            D_solver.zero_grad()
-            
+            _, input_channels, img_size, _ = x.shape
             real_images = preprocess_img(x).to(device)
+
+            # Discriminator step
+            D.zero_grad()
+            noise = sample_noise(batch_size, noise_size).to(device)
+            fake_images = G(noise).view(batch_size, input_channels, img_size, img_size)
             logits_real = D(real_images)
-            
-            g_fake_seed = sample_noise(batch_size, noise_size)
-            g_fake_seed = g_fake_seed.to(device)
-            fake_images = G(g_fake_seed).detach()
-            logits_fake = D(fake_images)
-            
-            # Calculate discriminator loss and take optimizer step
-            d_total_error = discriminator_loss(logits_real, logits_fake)
-            d_total_error.backward()
+            logits_fake = D(fake_images.detach())  # Detach to avoid training G on these gradients
+
+            d_error = discriminator_loss(logits_real, logits_fake)
+            d_error.backward()
             D_solver.step()
-            
+
             # Generator step
-            G_solver.zero_grad()
-            g_fake_seed = sample_noise(batch_size, noise_size)
-            g_fake_seed = g_fake_seed.to(device)
-            fake_images = G(g_fake_seed)
-            
-            # Recalculate discriminator logits on fake images
-            gen_logits_fake = D(fake_images)
-            g_error = generator_loss(gen_logits_fake)  # calculate generator loss
+            G.zero_grad()
+            logits_fake = D(fake_images)
+            g_error = generator_loss(logits_fake)
             g_error.backward()
             G_solver.step()
-            
-            d_error = d_total_error.item()
-            g_error = g_error.item()
-            
+
+            # Logging and output visualization
             if iter_count % show_every == 0:
-                print(f"Iter: {iter_count}, D: {d_error:.4f}, G:{g_error:.4f}")
-                fake_images = fake_images.view(batch_size, input_channels, img_size, img_size)
-                fake_images = deprocess_img(fake_images.data.cpu())
-                show_images(fake_images[0:16], color=input_channels != 1)
+                print(
+                    "Iter: {}, D: {:.4}, G:{:.4}".format(
+                        iter_count, d_error.item(), g_error.item()
+                    )
+                )
+                disp_fake_images = deprocess_img(fake_images.data)
+                imgs_numpy = disp_fake_images.cpu().numpy()
+                show_images(imgs_numpy[0:16], color=input_channels != 1)
                 plt.show()
-                print()
-            
             iter_count += 1
