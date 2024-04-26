313c5b303c8382cdc2114198414fc80642ae0fbe
HariniAru <35876094+HariniAru@users.noreply.github.com>
2024-04-26 18:08:12 -0500

    Add files via upload

--- assignment5_materials/config.py
+++ assignment5_materials/config.py
@@ -4,15 +4,13 @@ HEIGHT = 84
 WIDTH = 84
 HISTORY_SIZE = 4
 learning_rate = 0.0001
+lstm_seq_length = 20
 evaluation_reward_length = 100
 Memory_capacity = 1000000
-train_frame = 100000 # You can set it to a lower value while testing your code so you don't have to wait longer to see if the training code does not have any syntax errors
-batch_size = 128
+train_frame = 100000
+batch_size = 32
 scheduler_gamma = 0.4
 scheduler_step_size = 100000
 
 # Hyperparameters for Double DQN agent
 update_target_network_frequency = 1000
-
-# Hyperparameters for DQN LSTM agent
-lstm_seq_length = 5
